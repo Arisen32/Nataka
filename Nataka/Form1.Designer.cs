eb2c6a37fe621fd30a49410a84e4a36c728a0a99
??? <urielgutierrez490@gmail.com>
2024-09-24 23:08:22 -0600

    Add project files.

---
+++ Nataka/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Nataka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Login_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Login_password = new System.Windows.Forms.TextBox();
+            this.click_btnexit = new System.Windows.Forms.Label();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login Account";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Login_username
+            // 
+            this.Login_username.Location = new System.Drawing.Point(27, 168);
+            this.Login_username.Name = "Login_username";
+            this.Login_username.Size = new System.Drawing.Size(323, 22);
+            this.Login_username.TabIndex = 2;
+            this.Login_username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // Login_password
+            // 
+            this.Login_password.Location = new System.Drawing.Point(27, 234);
+            this.Login_password.Name = "Login_password";
+            this.Login_password.Size = new System.Drawing.Size(323, 22);
+            this.Login_password.TabIndex = 5;
+            // 
+            // click_btnexit
+            // 
+            this.click_btnexit.AutoSize = true;
+            this.click_btnexit.Location = new System.Drawing.Point(467, 9);
+            this.click_btnexit.Name = "click_btnexit";
+            this.click_btnexit.Size = new System.Drawing.Size(15, 16);
+            this.click_btnexit.TabIndex = 6;
+            this.click_btnexit.Text = "X";
+            this.click_btnexit.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.BackColor = System.Drawing.Color.MediumBlue;
+            this.Login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Login_btn.Location = new System.Drawing.Point(12, 399);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(106, 39);
+            this.Login_btn.TabIndex = 7;
+            this.Login_btn.Text = "Login";
+            this.Login_btn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(494, 450);
+            this.Controls.Add(this.Login_btn);
+            this.Controls.Add(this.click_btnexit);
+            this.Controls.Add(this.Login_password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Login_username);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Login_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Login_password;
+        private System.Windows.Forms.Label click_btnexit;
+        private System.Windows.Forms.Button Login_btn;
+    }
+}
+
---
+++ Nataka/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using OAuth2.Client;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+using OAuth2.Client.Impl;
+
+namespace Nataka
+{
+    public partial class Form1 : Form
+    {
+        private readonly GitHubClient _gitHubClient;
+        public Form1()
+        {
+            InitializeComponent();
+            var clientConfiguration = new ClientConfiguration();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //Label username
+            
+            Console.WriteLine("usuario");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //Label password
+            Console.WriteLine("contraseña");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
